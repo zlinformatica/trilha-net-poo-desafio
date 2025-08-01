0203a8ac6a81c7958aed50a5e15273376dd820f0
Jose Luiz Dos Santos <joseluizdossantos.ti@gmail.com>
2025-07-31 23:14:54 -0300

    Desafio Net POO concluido

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // <<<IMPLEMENTADO>>>
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria) { }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        // <<<IMPLEMENTADO>>>
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nome} no Nokia... ");
+        }
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,21 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // <<<IMPLEMENTADO>>>
+        public string Numero { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+        // TODO: Passar os parâmetros do construtor para as propriedades
+        // <<<IMPLEMENTADO>>>
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -21,6 +29,6 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        public abstract void InstalarAplicativo(string nome);
     }
 }
